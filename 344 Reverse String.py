1d74417c74c2095776d47f875d1b8514e1302d6e
AlexQianYi <q563875735@gmail.com>
2018-06-15 14:48:57 +0800

    344

---
+++ 344 Reverse String.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 15 14:48:27 2018
+
+@author: yiqian
+"""
+
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
+    
