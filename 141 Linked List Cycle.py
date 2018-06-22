bf60eb9723ecd366a53ffc289a1e3d06f3e3f19c
AlexQianYi <q563875735@gmail.com>
2018-06-22 15:49:20 +0800

    141

---
+++ 141 Linked List Cycle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 15:47:31 2018
+
+@author: yiqian
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return False
+    
+        slow = head
+        if head.next:
+            fast = head.next
+        else:
+            return False
+        
+        while slow is not fast:
+            if slow.next:
+                slow = slow.next
+            else:
+                return False
+            if not fast.next or not fast.next.next:
+                return False
+            else:
+                fast = fast.next.next
+
+        return True
+    
+    """
+    determine whether there is a cycle in linked list
+    two pointers, one fast one slow
+    """
