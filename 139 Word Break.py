7cf665ce2b6f7e9a2ed8b2ca8517f34f1206ba8e
AlexQianYi <q563875735@gmail.com>
2018-06-25 16:37:11 +0800

    139

---
+++ 139 Word Break.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 25 16:36:57 2018
+
+@author: yiqian
+"""
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: Set[str]
+        :rtype: bool
+        """
+        dp = [False] * (len(s) + 1) # dp[i] means s[:i+1] can be segmented into words in the wordDicts 
+        dp[0] = True
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if dp[i] and s[i: j+1] in wordDict:
+                    dp[j+1] = True
+                    
+        return dp[-1]
