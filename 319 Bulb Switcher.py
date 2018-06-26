c410933db212347b0e99db6e2db0da68b2dc4c7d
AlexQianYi <q563875735@gmail.com>
2018-06-26 10:47:46 +0800

    319

---
+++ 319 Bulb Switcher.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 26 10:45:26 2018
+
+@author: yiqian
+"""
+
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(math.sqrt(n))
+    
+    
+    """
+    A bulb ends up on iff it is switched an odd number of times.
+    Call them bulb 1 to bulb n. Bulb i is switched in round d if and only if d divides i. 
+    So bulb i ends up on if and only if it has an odd number of divisors.
+    Divisors come in pairs, like i=12 has divisors 1 and 12, 2 and 6, and 3 and 4. Except when i is a square, 
+    like 36 has divisors 1 and 36, 2 and 18, 3 and 12, 4 and 9, and double divisor 6. 
+    So bulb i ends up on if and only if i is a square.
+    So just count the square numbers.
+    Let R = int(sqrt(n)). That's the root of the largest square in the range [1,n]. 
+    And 1 is the smallest root. So you have the roots from 1 to R, that's R roots. 
+    Which correspond to the R squares. So int(sqrt(n)) is the answer. 
+    (C++ does the conversion to int automatically, because of the specified return type).
+    """
