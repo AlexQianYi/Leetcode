bd9ed1b343ce164c6e2f8b45b8897583f50e3dc3
AlexQianYi <q563875735@gmail.com>
2018-07-09 11:21:34 +0800

    79

---
+++ 79 Word Search.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  9 11:21:03 2018
+
+@author: yiqian
+"""
+
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        if not word:
+            return True
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.exist_helper(board, word, i, j):
+                    return True
+        return False
+                    
+    def exist_helper(self, board, word, i, j):
+        if board[i][j] == word[0]:
+            if not word[1:]:
+                return True
+            board[i][j] = " " # indicate used cell
+            # check all adjacent cells
+            if i > 0 and self.exist_helper(board, word[1:], i-1, j):
+                return True
+            if i < len(board)-1 and self.exist_helper(board, word[1:], i+1, j):
+                return True
+            if j > 0 and self.exist_helper(board, word[1:], i, j-1):
+                return True
+            if j < len(board[0])-1 and self.exist_helper(board, word[1:], i, j+1):
+                return True
+            board[i][j] = word[0] # update the cell to its original value
+            return False
+        else:
+            return False
