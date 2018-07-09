d700b0ecd91158a5089322e0155f0f324f92b58c
AlexQianYi <q563875735@gmail.com>
2018-07-09 17:03:59 +0800

    669

---
+++ 669 Trim a Binary Search Tree.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  9 17:03:35 2018
+
+@author: yiqian
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        
+        if root.val<L:
+            return self.trimBST(root.right, L, R)
+        elif root.val>R:
+            return self.trimBST(root.left, L, R)
+        else:
+            root.left = self.trimBST(root.left, L, R)
+            root.right = self.trimBST(root.right, L, R)
+        return root
