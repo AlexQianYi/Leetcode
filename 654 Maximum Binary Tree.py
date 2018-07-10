804415d38557b01e7266d532ed7c72f4abbfbef4
AlexQianYi <q563875735@gmail.com>
2018-07-10 14:19:14 +0800

    654

--- 654 Maximum Binary Tree.py
+++ 654 Maximum Binary Tree.py
@@ -29,3 +29,8 @@ class Solution(object):
         root.left = self.constructMaximumBinaryTree(nums[:max_index])
         root.right = self.constructMaximumBinaryTree(nums[max_index+1:])
         return root
+    
+    
+    """
+    beats 98%
+    """
