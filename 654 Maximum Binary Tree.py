191836f5e9002aa9d36fecb5449e5b6e12500b46
AlexQianYi <q563875735@gmail.com>
2018-07-10 14:18:54 +0800

    654

---
+++ 654 Maximum Binary Tree.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 10 14:18:32 2018
+
+@author: yiqian
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def constructMaximumBinaryTree(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return
+        max_index = 0
+        for i in xrange(1, len(nums)):
+            if nums[i] > nums[max_index]:
+                max_index = i
+        root = TreeNode(nums[max_index])
+        root.left = self.constructMaximumBinaryTree(nums[:max_index])
+        root.right = self.constructMaximumBinaryTree(nums[max_index+1:])
+        return root
