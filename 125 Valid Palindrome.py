1969c434a0f75b34c465d649a9919ce0f498a9de
AlexQianYi <q563875735@gmail.com>
2018-07-13 15:42:45 +0800

    125

---
+++ 125 Valid Palindrome.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 13 15:42:01 2018
+
+@author: yiqian
+"""
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l <r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l +=1; r -= 1
+        return True
