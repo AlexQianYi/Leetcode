c1f40a92d234df68d8d155b833aa32dfc0710aca
AlexQianYi <q563875735@gmail.com>
2018-07-26 10:43:24 +0800

    474

--- 474 Ones and Zeros.py
+++ 474 Ones and Zeros.py
@@ -7,27 +7,32 @@ Created on Wed Jul 25 16:44:08 2018
 """
 
         
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
         """
-        zeroNum, oneNum = self.cost(strs[0], 0), self.cost(strs[0], 1)
-        if len(strs) == 1:
-            if zeroNum<=m and oneNum<=n:
-                return 1
-            else:
-                return 0
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        p=[[0 for x in range(n+1)] for t in range(m+1)]
+        for s in strs:
+            cost_zero,cost_one=self.cost(s)
+            
+            
+            for i in range (m,cost_zero-1,-1):
+                for j in range(n,cost_one-1,-1):
+                    
+                    p[i][j]=max(p[i][j],p[i-cost_zero][j-cost_one]+1)
+        return p[m][n]
         
-        if zeroNum<=m and oneNum<=n:
-            return max(1+self.findMaxForm(strs[1:], m-zeroNum, n-oneNum), self.findMaxForm(strs[1:], m, n))
-        else:
-            return self.findMaxForm(strs[1:], m, n)
-    
-    def cost(self,s, num):
-        count = 0
+        
+    def cost(self,s):
+        cout_zero=0
+        cout_one=0
         for i in range(len(s)):
-            if s[i]==str(num):
-                count += 1
-        return count 
-        """
-        
-        """
-        beautiful recursion, but time out limiation
-        """
+            if s[i]=='0':
+                cout_zero+=1
+            elif s[i]=="1":
+                cout_one+=1
+        return cout_zero,cout_one
