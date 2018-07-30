5262fc0307a6190d57d0884b8ca8b7888479aa7b
AlexQianYi <q563875735@gmail.com>
2018-07-30 09:53:29 +0800

    92

---
+++ 92 Reverse Linked List II.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 09:53:13 2018
+
+@author: yiqian
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        if m == n:
+            return head
+
+        dummyNode = ListNode(0)
+        dummyNode.next = head
+        pre = dummyNode
+
+        for i in range(m - 1):
+            pre = pre.next
+        
+        # reverse the [m, n] nodes
+        reverse = None
+        cur = pre.next
+        for i in range(n - m + 1):
+            next = cur.next
+            cur.next = reverse
+            reverse = cur
+            cur = next
+
+        pre.next.next = cur
+        pre.next = reverse
+
+        return dummyNode.next
