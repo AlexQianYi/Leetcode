70d5a7a569366bb69b54eb7b7028b6e17be56b36
AlexQianYi <q563875735@gmail.com>
2018-07-30 09:21:48 +0800

    75

---
+++ 75 Sort Colors.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 09:21:34 2018
+
+@author: yiqian
+"""
+
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        i = j = 0
+        for k in xrange(len(nums)):
+            v = nums[k]
+            nums[k] = 2
+            if v < 2:
+                nums[j] = 1
+                j += 1
+            if v == 0:
+                nums[i] = 0
+                i += 1
