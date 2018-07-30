e463ec39ee3f1249aad418c2a8a138300052680a
AlexQianYi <q563875735@gmail.com>
2018-07-30 14:51:52 +0800

    253

---
+++ 253 Meeting Rooms II.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 14:51:20 2018
+
+@author: yiqian
+"""
+
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+        starts = []
+        ends = []
+        for i in intervals:
+            starts.append(i.start)
+            ends.append(i.end)
+        
+        starts.sort()
+        ends.sort()
+        s = e = 0
+        numRooms = available = 0
+        while s < len(starts):
+            if starts[s] < ends[e]:
+                if available == 0:
+                    numRooms += 1
+                else:
+                    available -= 1
+                    
+                s += 1
+            else:
+                available += 1
+                e += 1
+        
+        return numRooms
